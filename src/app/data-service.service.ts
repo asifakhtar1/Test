0b2d0df9467176af97d6dd569c294c00e5ade9aa
asifakhtar1@yahoo.com <asifakhtar1@yahoo.com>
2021-09-02 16:36:54 -0400

    Fix formatting

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -6,43 +6,43 @@ import { catchError, map, tap } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
-export class DataServiceService {
 
+export class DataServiceService {
   constructor(private http: HttpClient) { }
 
-  public getDeezerArtistData$(name: string) : Observable<any> {    
-   const requestedParams = {
-     q: name
-   }; 
+  public getDeezerArtistData$(name: string): Observable<any> {
+    const requestedParams = {
+      q: name
+    };
 
-   return this.http
-   .get('https://api.deezer.com/search', {
-     observe: 'response',
-     params: requestedParams
-   })
-   .pipe(     
-    catchError(err => {
-      return of(null)
-    })
-   );
+    return this.http
+      .get('https://api.deezer.com/search', {
+        observe: 'response',
+        params: requestedParams
+      })
+      .pipe(
+        catchError(err => {
+          return of(null)
+        })
+      );
   }
 
-  public getiTunesArtistData$(name: string) : Observable<any> {    
-   const requestedParams = {
-    term: name
-   }; 
+  public getiTunesArtistData$(name: string): Observable<any> {
+    const requestedParams = {
+      term: name
+    };
 
-   return this.http
-   .get('https://itunes.apple.com/search', {
-     observe: 'response',
-     params: requestedParams
-   })
-   .pipe(
-     map(( { body }: any) => body.results),
-     catchError(err => {
-       return of(null)
-     })
-   )
+    return this.http
+      .get('https://itunes.apple.com/search', {
+        observe: 'response',
+        params: requestedParams
+      })
+      .pipe(
+        map(({ body }: any) => body.results),
+        catchError(err => {
+          return of(null)
+        })
+      )
   }
 
 }
